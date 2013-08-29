b3c8b9f7ee04ded7cf9b3630d0bcab037ea92fb2
qus <qus@tut.by>
2013-08-29 20:45:47 +0300

    Fix comments for PercolationStats

--- Algorithms1/src/Percolation.java
+++ Algorithms1/src/Percolation.java
@@ -3,90 +3,99 @@
  */
 //import WeightedQuickUnionUF;
 
-
 public class Percolation {
-	private WeightedQuickUnionUF wqfItems;
-	private boolean[] wqfItemStatus;
-	private int iEleNumTotal;
-	private int iN;
+    private WeightedQuickUnionUF wqfItems;
+    private boolean[] wqfItemStatus;
+    private int iEleNumTotal;
+    private int iN;
 
-	public Percolation(int N){              // create N-by-N grid, with all sites blocked
-	   int i;	   
-	   iEleNumTotal = N*N + 2;
-	   iN = N;
+    public Percolation(int N) { // create N-by-N grid, with all sites blocked
+	int i;
+	iEleNumTotal = N * N + 2;
+	iN = N;
 
-	   wqfItems = new WeightedQuickUnionUF(iEleNumTotal);
-	   wqfItemStatus = new boolean[iEleNumTotal];
-	   for (i = 0 ; i < iEleNumTotal; i ++){
-		   wqfItemStatus[i] = false;
-	   }
-	   wqfItemStatus[iEleNumTotal - 2] = true;
-	   wqfItemStatus[iEleNumTotal - 1] = true;
-	   
+	wqfItems = new WeightedQuickUnionUF(iEleNumTotal);
+	wqfItemStatus = new boolean[iEleNumTotal];
+	for (i = 0; i < iEleNumTotal; i++) {
+	    wqfItemStatus[i] = false;
 	}
-	public void open(int i, int j){         // open site (row i, column j) if it is not already
-	   int iElementId;
-	   if (i < 1 || i > iN) throw new IndexOutOfBoundsException("row index i out of bounds");
-	   if (j < 1 || j > iN) throw new IndexOutOfBoundsException("col index i out of bounds");
-	   
-	   i -= 1;
-	   j -= 1;
-	   iElementId = i*iN + j;
-	   
-	   if(wqfItemStatus[iElementId]){
-		   return;
-	   }
+	wqfItemStatus[iEleNumTotal - 2] = true;
+	wqfItemStatus[iEleNumTotal - 1] = true;
 
-	   //   /\
-	   //   || 
-	   if(i>0){
-		   if(wqfItemStatus[(i-1)*iN + j]){
-			   wqfItems.union((i-1)* iN + j, iElementId);
-		   }
-	   }else{
-		   wqfItems.union(iEleNumTotal - 2, iElementId);
-	   }
-	   
-	   
-	   //   ||
-	   //   \/ 
-	   if(i<iN - 1){
-		   if(wqfItemStatus[(i+1)* iN + j]){
-			   wqfItems.union((i+1)* iN + j, iElementId);   
-		   }
-		   
-	   }else{
-		   wqfItems.union(iEleNumTotal - 1, iElementId);
-	   }
-	   
-	   //   <--
-	   if(j>0){
-		   if(wqfItemStatus[i* iN + j - 1]){
-			   wqfItems.union(i* iN + j - 1, iElementId);
-		   }
-	   }
-	   //   -->
-	   if(j<iN-1){
-		   if(wqfItemStatus[i* iN + j + 1]){
-			   wqfItems.union(i* iN + j + 1, iElementId);
-		   }
-	   }
-	   wqfItemStatus[iElementId] = true;
-	   return;
-	   
+    }
+
+    public void open(int i, int j) { // open site (row i, column j) if it is not
+				     // already
+	int iElementId;
+	if (i < 1 || i > iN)
+	    throw new IndexOutOfBoundsException("row index i out of bounds");
+	if (j < 1 || j > iN)
+	    throw new IndexOutOfBoundsException("col index i out of bounds");
+
+	i -= 1;
+	j -= 1;
+	iElementId = i * iN + j;
+
+	if (wqfItemStatus[iElementId]) {
+	    return;
 	}
-	public boolean isOpen(int i, int j){    // is site (row i, column j) open?
-	   if (i < 1 || i > iN) throw new IndexOutOfBoundsException("row index i out of bounds");
-	   if (j < 1 || j > iN) throw new IndexOutOfBoundsException("col index i out of bounds");
-	   
-	   return wqfItemStatus[(i-1)*iN + j-1];
+
+	// /\
+	// ||
+	if (i > 0) {
+	    if (wqfItemStatus[(i - 1) * iN + j]) {
+		wqfItems.union((i - 1) * iN + j, iElementId);
+	    }
+	} else {
+	    wqfItems.union(iEleNumTotal - 2, iElementId);
 	}
-	public boolean isFull(int i, int j){    // is site (row i, column j) full?
-	   if (i < 1 || i > iN) throw new IndexOutOfBoundsException("row index i out of bounds");
-	   if (j < 1 || j > iN) throw new IndexOutOfBoundsException("col index i out of bounds");
-	   return wqfItems.connected(iEleNumTotal-2, (i-1)*iN + j-1);
+
+	// ||
+	// \/
+	if (i < iN - 1) {
+	    if (wqfItemStatus[(i + 1) * iN + j]) {
+		wqfItems.union((i + 1) * iN + j, iElementId);
+	    }
+
+	} else {
+	    wqfItems.union(iEleNumTotal - 1, iElementId);
 	}
-	public boolean percolates(){            // does the system percolate?
-	   return wqfItems.connected(iEleNumTotal-2, iEleNumTotal-1);
+
+	// <--
+	if (j > 0) {
+	    if (wqfItemStatus[i * iN + j - 1]) {
+		wqfItems.union(i * iN + j - 1, iElementId);
+	    }
 	}
+	// -->
+	if (j < iN - 1) {
+	    if (wqfItemStatus[i * iN + j + 1]) {
+		wqfItems.union(i * iN + j + 1, iElementId);
+	    }
+	}
+	wqfItemStatus[iElementId] = true;
+	return;
+
+    }
+
+    public boolean isOpen(int i, int j) { // is site (row i, column j) open?
+	if (i < 1 || i > iN)
+	    throw new IndexOutOfBoundsException("row index i out of bounds");
+	if (j < 1 || j > iN)
+	    throw new IndexOutOfBoundsException("col index i out of bounds");
+
+	return wqfItemStatus[(i - 1) * iN + j - 1];
+    }
+
+    public boolean isFull(int i, int j) { // is site (row i, column j) full?
+	if (i < 1 || i > iN)
+	    throw new IndexOutOfBoundsException("row index i out of bounds");
+	if (j < 1 || j > iN)
+	    throw new IndexOutOfBoundsException("col index i out of bounds");
+	return wqfItems.connected(iEleNumTotal - 2, (i - 1) * iN + j - 1);
+    }
+
+    public boolean percolates() { // does the system percolate?
+	return wqfItems.connected(iEleNumTotal - 2, iEleNumTotal - 1);
+    }
 }
--- Algorithms1/src/PercolationStats.java
+++ Algorithms1/src/PercolationStats.java
@@ -1,88 +1,129 @@
-//import Percolation
-//import StdRandom
+/*----------------------------------------------------------------
+ *  Author:        Andrei Perapiolkin
+ *  Written:       28/8/2013
+ *  Last updated:  28/8/2013
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats
+ *  
+ *  Use class Percolation to solve problem of finding number of
+ *  random inserts in square size N*N to make it percolates from 
+ *  top to bottom.
+ *
+ *  java PercolationStats 200 100
+ *  mean                    = 0.5929934999999997
+ *  stddev                  = 0.00876990421552567
+ *  95% confidence interval = 0.5912745987737567, 0.5947124012262428
+ *
+ *----------------------------------------------------------------*/
 
 public class PercolationStats {
-//	private int iNSize;
-//	private int iTimesNum;
-	private double[] iExpResX;
-	private double perstMean;
-	private double perstStdDev;
-	private double perstConfHi;
-	private double perstConfLo;
-	
-	public PercolationStats(int N, int T){    // perform T independent computational experiments on an N-by-N grid
-		//iNSize = N;
-		//iTimesNum = T;
-		double dTmp = 0;
-		//double dTime;
-		int i = 0;
-		int iOpen;
-		int x,y;
-		if(N <= 0 || T <= 0) throw new java.lang.IllegalArgumentException();
-		iExpResX = new double[T];
-		for (i = 0; i < T ; i ++){
-			iOpen = 0;
-			Percolation pPercExp = new Percolation(N);
-			while(true){
-				x = StdRandom.uniform(1, N + 1);
-				y = StdRandom.uniform(1, N + 1);
-				if( pPercExp.isOpen(x, y) ){
-					continue;
-				}
-				pPercExp.open(x, y);
-				iOpen += 1;				
-				if( pPercExp.percolates() ){
-					break;
-				}
-			}
+    private double[] iExpResX; 		// Experiment result log
+    private double perstMean; 		// Mean calculated value
+    private double perstStdDev; 	// Standard deviation comp. value
+    private double perstConfHi; 	// Confidence interval high value
+    private double perstConfLo; 	// Confidence interval low value
 
-			iExpResX[i] = iOpen/(Math.pow(N,2));
-		}
-		
-		for(i = 0 ; i < T ; i ++){
-			dTmp += iExpResX[i];	
-		}
-		perstMean = dTmp / T;
-		
-		dTmp = 0;
-		
-		for(i = 0 ; i < T ; i ++){
-			dTmp += Math.pow(iExpResX[i] - perstMean,2);
-		}
-		perstStdDev = dTmp / (T - 1);
-		
-		perstConfHi = perstMean + (1.96 * perstStdDev)/Math.sqrt(T);
-		perstConfLo = perstMean - (1.96 * perstStdDev)/Math.sqrt(T);
-		
-		
-//		Stopwatch tWatch = new Stopwatch(); // init
-//		dTime = tWatch.elapsedTime();// get elapsed time
-		
-	}
-	public double mean(){                     // sample mean of percolation threshold
-		return perstMean;
-		
-	}
-	public double stddev(){                   // sample standard deviation of percolation threshold
-		return Math.sqrt(perstStdDev);
-	}
-	public double confidenceLo(){             // returns lower bound of the 95% confidence interval
-		return perstConfLo;
-	}
-	public double confidenceHi(){             // returns upper bound of the 95% confidence interval
-		return perstConfHi;
-	}
-	public static void main(String[] args){   // test client, described below
-		int N,T;
-		PercolationStats psCurExp;
-		if(args.length < 2) throw new IndexOutOfBoundsException("Not enongh arguments!\n");
-		N = Integer.parseInt(args[0]);
-        T = Integer.parseInt(args[1]);
-        psCurExp = new PercolationStats( N, T);
+    /**
+     * By creating an object of PercolationStats
+     * perform T independent computational experiments on an N-by-N grid
+     * 
+     * Throws a IllegalArgumentException if N or T is less then 0.
+     */
+    public PercolationStats(int N, int T) { 
 
-        StdOut.println("mean                    = " + psCurExp.mean());
-        StdOut.println("stddev                  = " + psCurExp.stddev());
-        StdOut.println("95% confidence interval = " +  psCurExp.confidenceLo() + " " + psCurExp.confidenceHi());
-		//StdRandom.uniform(a, b)
+	double dTmp = 0;		// Tmp double storage
+
+	int i = 0;			// int iterator
+	int iOpen;
+	int x, y;
+	if (N <= 0 || T <= 0)
+	    throw new java.lang.IllegalArgumentException();
+	iExpResX = new double[T];
+	for (i = 0; i < T; i++) {	//  Fill iExpResX[i] with experiment 
+	    				//        data on the cube size N*N
+	    iOpen = 0;			//  num of random open before percolation
+	    Percolation pPercExp = new Percolation(N);
+	    while (true) {
+		x = StdRandom.uniform(1, N + 1);
+		y = StdRandom.uniform(1, N + 1);
+		if (pPercExp.isOpen(x, y)) {
+		    continue;
+		}
+		pPercExp.open(x, y);
+		iOpen += 1;
+		if (pPercExp.percolates()) {
+		    break;
+		}
+	    }
+	    iExpResX[i] = iOpen / (Math.pow(N, 2));
 	}
+
+	for (i = 0; i < T; i++) { 	//  Sum all exp proportional results
+	    dTmp += iExpResX[i];
+	}
+	perstMean = dTmp / T;		//  Calculate mean
+
+	dTmp = 0;
+
+	for (i = 0; i < T; i++) {
+	    dTmp += Math.pow(iExpResX[i] - perstMean, 2);
+	}
+	perstStdDev = dTmp / (T - 1);	//  Calculate standard deviation
+
+	/*
+	 * Confidential interval
+	 */
+	perstConfHi = perstMean + (1.96 * perstStdDev) / Math.sqrt(T); 
+	perstConfLo = perstMean - (1.96 * perstStdDev) / Math.sqrt(T);
+
+    }
+    
+    /**
+     * sample mean of percolation threshold
+     * @return double mean
+     */
+    public double mean() { // 
+	return perstMean;
+
+    }
+    
+    /**
+     * sample standard deviation of percolation threshold
+     * @return  standard deviation value
+     */
+    public double stddev() { 
+	return Math.sqrt(perstStdDev);
+    }
+
+    /**
+     * @returns lower bound of the 95% confidence interval
+     */
+    public double confidenceLo() { 
+				   
+	return perstConfLo;
+    }
+
+    /**
+     * @returns upper bound of the 95% confidence interval
+     */
+    public double confidenceHi() { 
+				   
+	return perstConfHi;
+    }
+
+    public static void main(String[] args) { 
+	int N, T;
+	PercolationStats psCurExp;
+	if (args.length < 2)
+	    throw new IndexOutOfBoundsException("Not enongh arguments!\n");
+	N = Integer.parseInt(args[0]);
+	T = Integer.parseInt(args[1]);
+	psCurExp = new PercolationStats(N, T);
+
+	StdOut.println("mean                    = " + psCurExp.mean());
+	StdOut.println("stddev                  = " + psCurExp.stddev());
+	StdOut.println("95% confidence interval = " + psCurExp.confidenceLo()
+		+ " " + psCurExp.confidenceHi());
+    }
 }
